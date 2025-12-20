9bd64b1ec1f66d87458321b2f3d3dc157ac1b9e7
Jay <dhananjay.rughani@gmail.com>
2025-12-20 22:49:03 +0400

    admin_routes.py not working debug here 20122025-2249

--- app/routes/admin_routes.py
+++ app/routes/admin_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, session, redirect, url_for
 from db import get_db_connection
 
+print("ADMIN ROUTES LOADED")
 admin_bp = Blueprint("admin", __name__)
 
 @admin_bp.route("/requests")
