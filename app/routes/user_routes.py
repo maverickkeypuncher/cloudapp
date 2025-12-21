01af6b37530db5e6ce53e673ca8aba8a8689bc34
Jay <dhananjay.rughani@gmail.com>
2025-12-21 11:23:49 +0400

    added missing comma in user_routes 21122025-1123

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -46,7 +46,7 @@ def form():
         "form.html",
         datacenters=["DUBAI", "ABU DHABI"],
         num_servers_options=[1,2,3,4,5,6,7,8,9,10],
-        storage_options=["100GB", "200GB", "500GB", "1024GB", "2048GB"]
+        storage_options=["100GB", "200GB", "500GB", "1024GB", "2048GB"],
         operating_system=["Windows Server","RedHat Enterprise Linux"]
     )
 
